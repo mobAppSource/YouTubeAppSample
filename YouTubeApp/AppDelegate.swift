cf279e65f3fabefcd986e3ae78a511c9717b0599
MacOS <macos@macoss-mac-2.local>
2016-07-08 01:56:32 +0200

    Swipe views

--- YouTubeApp/AppDelegate.swift
+++ YouTubeApp/AppDelegate.swift
@@ -19,6 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window?.makeKeyAndVisible()
         
         let layout = UICollectionViewFlowLayout()
+//        layout.scrollDirection = .Horizontal
         window?.rootViewController = UINavigationController(rootViewController: HomeController(collectionViewLayout: layout))
         UINavigationBar.appearance().barTintColor = UIColor.rgb(230, green: 32, blue: 31, alpha: 1)
         //Adding menu item images from
